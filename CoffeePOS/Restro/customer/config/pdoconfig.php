606555fb63c1bc916118a894422b828758e354fd
yuzffff <tanakorn.moteh@g.swu.ac.th>
2024-05-12 17:52:50 +0700

    Change root

--- CoffeePOS/Restro/customer/config/pdoconfig.php
+++ CoffeePOS/Restro/customer/config/pdoconfig.php
@@ -1,7 +1,7 @@
 <?php
 $DB_host = "localhost";
 $DB_user = "root";
-$DB_pass = "";
+$DB_pass = "root";
 $DB_name = "rposystem";
 try {
     $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}", $DB_user, $DB_pass);
