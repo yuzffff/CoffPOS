f17c6b2f21cf9ad507618d94e5300f82deabafc5
yuzffff <tanakorn.moteh@g.swu.ac.th>
2024-05-12 23:00:29 +0700

    Delete Customer index.php

--- CoffeePOS/index.php
+++ CoffeePOS/index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<!-- For more projects: Visit codeastro.com  -->
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -65,7 +64,6 @@
         }
     </style>
 </head>
-<!-- For more projects: Visit codeastro.com  -->
 <body>
     <div class="flex-center position-ref full-height">
         <div class="content">
@@ -74,13 +72,10 @@
             </div>
 
             <div class="links">
-			<!-- For more projects: Visit codeastro.com  -->
                 <a href="Restro/admin/">Admin Log In</a>
                 <a href="Restro/cashier/">Cashier Log In</a>
-                <a href="Restro/customer">Customer Log In</a>
             </div>
         </div>
     </div>
 </body>
-<!-- For more projects: Visit codeastro.com  -->
 </html>
