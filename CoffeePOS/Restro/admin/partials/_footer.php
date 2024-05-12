9ccc9331afc424d59f269971152e60402871ac41
Taotaoz040746 <Nattawutbootta154@gmail.com>
2024-05-12 22:13:35 +0700

    E._footer

--- CoffeePOS/Restro/admin/partials/_footer.php
+++ CoffeePOS/Restro/admin/partials/_footer.php
@@ -9,7 +9,7 @@
       <div class="col-xl-6">
         <ul class="nav nav-footer justify-content-center justify-content-xl-end">
           <li class="nav-item">
-            <a href="" class="nav-link" target="_blank">Restaurant POS</a>
+            <a href="" class="nav-link" target="_blank">Coffee POS</a>
           </li>
         </ul>
       </div>
