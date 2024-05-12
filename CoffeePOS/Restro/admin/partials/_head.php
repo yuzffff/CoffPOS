995a10b02634ff40fd46feb7f4e3df05d4ac56ad
Taotaoz040746 <Nattawutbootta154@gmail.com>
2024-05-12 22:16:31 +0700

    E._head

--- CoffeePOS/Restro/admin/partials/_head.php
+++ CoffeePOS/Restro/admin/partials/_head.php
@@ -25,7 +25,7 @@
     <script src="assets/js/swal.js"></script>
     <!--Load Swal-->
     <?php if (isset($success)) { ?>
-        <!--This code for injecting success alert-->
+        
         <script>
             setTimeout(function() {
                     swal("Success", "<?php echo $success; ?>", "success");
@@ -35,7 +35,7 @@
 
     <?php } ?>
     <?php if (isset($err)) { ?>
-        <!--This code for injecting error alert-->
+        <!--Codeชุดนี้สำหรับการแจ้งเตือนข้อผิดพลาดในการแทรก-->
         <script>
             setTimeout(function() {
                     swal("Failed", "<?php echo $err; ?>", "error");
@@ -45,7 +45,7 @@
 
     <?php } ?>
     <?php if (isset($info)) { ?>
-        <!--This code for injecting info alert-->
+        <!--Codeนี้สำหรับใส่การแจ้งเตือนข้อมูล-->
         <script>
             setTimeout(function() {
                     swal("Success", "<?php echo $info; ?>", "info");
