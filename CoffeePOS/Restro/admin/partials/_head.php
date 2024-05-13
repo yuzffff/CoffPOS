a77aee04f92ca63cd4e6d9a19eec1deefd2b0391
Taotaoz040746 <Nattawutbootta154@gmail.com>
2024-05-14 03:43:42 +0700

    E._head

--- CoffeePOS/Restro/admin/partials/_head.php
+++ CoffeePOS/Restro/admin/partials/_head.php
@@ -9,8 +9,6 @@
     <title>Coffee Point Of Sale </title>
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="assets/img/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/icons/favicon-16x16.png">
     <link rel="manifest" href="assets/img/icons/site.webmanifest">
     <link rel="mask-icon" href="assets/img/icons/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
