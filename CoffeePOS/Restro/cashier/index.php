08bee75de577825e7305e44f709628a51ff530e3
Srpk0ng <admin@admins-MacBook-Air.local>
2024-05-13 12:13:08 +0700

    edit delete index

--- CoffeePOS/Restro/cashier/index.php
+++ CoffeePOS/Restro/cashier/index.php
@@ -4,7 +4,7 @@ include('config/config.php');
 //login 
 if (isset($_POST['login'])) {
   $staff_email = $_POST['staff_email'];
-  $staff_password = sha1(md5($_POST['staff_password'])); //double encrypt to increase security
+  $staff_password = sha1(md5($_POST['staff_password'])); // เข้ารหัสสองครั้งเพื่อเพิ่มความปลอดภัย
   $stmt = $mysqli->prepare("SELECT staff_email, staff_password, staff_id  FROM   rpos_staff WHERE (staff_email =? AND staff_password =?)"); //sql to log in user
   $stmt->bind_param('ss',  $staff_email, $staff_password); //bind fetched parameters
   $stmt->execute(); //execute bind 
