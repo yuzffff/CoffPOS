ffa38fb4a1e0a7abfd7fe73f070157aab1489674
yuzffff <tanakorn.moteh@g.swu.ac.th>
2024-05-13 16:43:44 +0700

    E Add Product

--- CoffeePOS/Restro/admin/add_product.php
+++ CoffeePOS/Restro/admin/add_product.php
@@ -26,7 +26,7 @@ if (isset($_POST['addProduct'])) {
     $postStmt->execute();
     //ประกาศตัวแปรที่จะส่งไปฟังก์ชั่นการแจ้งเตือน
     if ($postStmt) {
-      $success = "Product Added" && header("refresh:1; url=add_product.php");
+      $success = "Product Added" && header("refresh:1; url=products.php");
     } else {
       $err = "Please Try Again Or Try Later";
     }
